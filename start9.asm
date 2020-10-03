5678eb3a452c61c9deddbaaa0bfc60d5976eea47
RonRud <31389934+RonRud@users.noreply.github.com>
2020-10-03 22:55:52 +0300

    Uploaded final project

---
+++ start9.asm
@@ -0,0 +1,626 @@
+;שם בית הספר: דה שליט
+;שם התלמיד: רון רודיך
+;שם המנחה: איזבלה טבלין
+;ת.ז: 326240603
+;שם חלופה: 899283
+
+IDEAL
+MODEL small
+STACK 100h
+DATASEG
+;map is 80,50 every map index represents 4/4
+map db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+	db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1
+	db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+	db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+	x dw ?
+	y dw ?
+	color db ?
+	
+	playerX dw 2
+	playerY dw 47
+	
+	jumpTurns db 5
+	gravityFlag db 0
+	turnsInAir db 0
+	
+	startfile db 'start.bmp',0
+	endfile db 'winer.bmp', 0
+	imgOffset db ?
+	imgHeight dw 200
+	imgWidth dw 320
+	adjustCX dw ?
+	filename db 20 dUp (?)
+	filehandle dw ?
+	Header db 54 dUp (0)
+	Palette db 256*4 dUp (0)
+	ScrLine db 320 dUp (0)
+	Errormsg db 'Error', 13, 10, '$'
+	printAdd dw ?
+	
+CODESEG
+
+;Prints the bmp file provided
+;IN: ax - img offset, imgHeight (dw), imgWidth (dw), printAdd (dw)
+;OUT: printed bmp file
+proc PrintBmp
+	push cx
+
+	push di
+	push si
+	push cx
+	push ax
+
+	
+	xor di, di
+	mov di, ax
+	mov si, offset filename
+	
+	mov cx, 20
+copy:
+	mov al, [di]
+	mov [si], al
+	inc di
+	inc si
+	loop copy
+	
+
+	pop ax
+	pop cx
+	pop si
+	pop di
+	
+	call OpenFile
+	call ReadHeader
+	call ReadPalette
+	call CopyPal
+	call CopyBitMap
+	call CloseFile
+	
+	pop cx
+	ret
+endp PrintBmp
+
+proc GraphicsMode
+	push ax
+	
+	mov ax, 13h
+	int 10h
+		
+	pop ax
+	ret
+endp GraphicsMode
+
+
+;in proc PrintBmp
+proc OpenFile
+	mov ah,3Dh
+	xor al,al ;for reading only
+	mov dx, offset filename
+	int 21h
+	jc OpenError
+	mov [filehandle],ax
+	ret
+OpenError:
+	mov dx,offset Errormsg
+	mov ah,9h
+	int 21h
+	ret
+endp OpenFile
+
+;in proc PrintBmp
+proc ReadHeader
+;Read BMP file header, 54 bytes
+	mov ah,3Fh
+	mov bx,[filehandle]
+	mov cx,54
+	mov dx,offset Header
+	int 21h
+	ret
+endp ReadHeader
+
+;in proc PrintBmp
+proc ReadPalette
+;Read BMP file color palette, 256 colors*4bytes for each (400h)
+	mov ah,3Fh
+	mov cx,400h
+	mov dx,offset Palette
+	int 21h
+	ret
+endp ReadPalette
+
+;in proc PrintBmp
+proc CopyPal
+; Copy the colors palette to the video memory
+; The number of the first color should be sent to port 3C8h
+; The palette is sent to port 3C9h
+	mov si,offset Palette
+	mov cx,256
+	mov dx,3C8h ;port of Graphics Card
+	mov al,0 ;number of first color
+	;Copy starting color to port 3C8h
+	out dx,al
+	;Copy palette itself to port 3C9h
+	inc dx
+PalLoop:
+	;Note: Colors in a BMP file are saved as BGR values rather than RGB.	
+	mov al,[si+2] ;get red value
+	shr al,2 	; Max. is 255, but video palette maximal value is 63. Therefore dividing by 4
+	out dx,al ;send it to port
+	mov al,[si +1];get green value
+	shr al,2
+	out dx,al	;send it
+	mov al,[si]
+	shr al,2
+	out dx,al 	;send it
+	add si,4	;Point to next color (There is a null chr. after every color)
+	loop PalLoop
+	ret
+endp CopyPal
+
+;in proc PrintBmp
+proc CopyBitMap
+; BMP graphics are saved Upside-down.
+; Read the graphic line by line ([height] lines in VGA format),
+; displaying the lines from bottom to top.
+	mov ax,0A000h ;value of start of video memory
+	mov es,ax
+	
+	push ax
+	push bx
+	mov ax, [imgWidth]
+	mov bx, 4
+	div bl
+	
+	cmp ah, 0
+	jne NotZero
+Zero:
+	mov [adjustCX], 0
+	jmp Continue
+NotZero:
+	mov [adjustCX], 4
+	xor bx, bx
+    mov bl, ah
+	sub [adjustCX], bx
+Continue:
+	pop bx
+	pop ax
+	
+	mov cx, [imgHeight]	;reading the BMP data - Upside down
+	
+PrintBMPLoop:
+	push cx
+	
+	xor di, di
+	push cx
+	dec cx
+	Multi:
+		add di, 320
+		loop Multi
+	pop cx
+
+    add di, [printAdd]
+
+	mov ah, 3fh
+	mov cx, [imgWidth]
+	add cx, [adjustCX]
+	mov dx, offset ScrLine
+	int 21h
+	;Copy one line into video memory
+	cld	;clear direction flag - due to the use of rep
+	mov cx, [imgWidth]
+	mov si, offset ScrLine
+	rep movsb 	;do cx times:
+				;mov es:di,ds:si -- copy single value form ScrLine to video memory
+				;inc si --inc - because of cld
+				;inc di --inc - because of cld
+	pop cx
+	loop PrintBMPLoop
+	ret
+endp CopyBitMap
+
+;in proc PrintBmp
+proc CloseFile
+	mov ah,3Eh
+	mov bx,[filehandle]
+	int 21h
+	ret
+endp CloseFile
+
+
+;Prints the win screen
+proc Win
+
+	mov ax, offset endfile
+	call PrintBmp
+	
+	;Wait for key press
+	mov ah,1
+	int 21h
+	
+	call PrintMap
+	ret
+endp Win
+
+
+;prints map dot (actual 4/4) before calling this method assaign:
+;	[x] to the starting x
+;	[y] to the starting y
+;	[color] to the color
+proc PrintMapDot 
+	push bx
+	push cx
+	push dx
+	push ax
+	mov dx, [y]
+	mov al, [color]
+
+	push ax
+	mov ax, 4
+	; multiplies x and y so they will match actual pixels
+	mov ax, 4
+	mul dx
+	mov dx, ax
+	pop ax
+	mov bl, 0;0 a must do
+	
+	mov ah, 0 ;counter for Yloop
+	
+YLoop:
+	mov bh, 0; resets for XLoop
+	mov cx, [x]
+	
+	push ax
+	push dx
+	mov ax, 4
+	mul cx; resets x position multiplie to match actual pixels
+	mov cx, ax
+	pop dx
+	pop ax
+	
+XLoop:
+	push ax ;makes sure ah doesnt change
+	mov ah, 0Ch ; prints pixel
+	int 10h
+	pop ax
+	
+	inc cx
+	
+	inc bh
+	cmp bh, 4
+	jne XLoop
+;acts after XLoop
+	inc dx
+
+	inc ah
+	cmp ah,4
+	jne YLoop
+
+;acts after YLoop
+	pop ax
+	pop dx
+	pop cx
+	pop bx
+	ret
+endp PrintMapDot
+
+
+;Prints the map array using MapDot proc
+;goes through every place in the array and prints white or black according to if the value is 0 or 1
+proc PrintMap
+	push ax
+	push bx
+
+	mov [color], 0fh
+	mov [y], 0
+	lea bx, [map]	
+	
+MapYLoop:
+	mov [x], 0
+
+MapXLoop:
+	cmp [byte ptr bx], 1
+	jne finish
+	
+	call PrintMapDot
+	
+	finish:
+		inc bx
+		inc [x]
+		cmp [x], 80 		
+		jne MapXLoop
+;happens after MapXLoop
+	inc [y]
+	cmp [y], 50
+	jne MapYLoop
+
+	pop bx
+	pop ax
+	ret
+endp PrintMap
+
+proc RemovePlayer
+	push ax
+	push bx
+	push cx
+	push dx
+
+	mov ax, [playerX]
+	mov [x], ax
+	mov ax, [playerY]
+	mov [y], ax
+	mov [color], 0
+	call PrintMapDot
+
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+endp RemovePlayer
+
+proc AddPlayer
+	push ax
+	push bx
+	push cx
+	push dx
+
+	mov ax, [playerX]
+	mov [x], ax
+	mov ax, [playerY]
+	mov [y], ax
+	mov [color], 2
+	call PrintMapDot
+
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+endp AddPlayer	
+
+;checks if a button is pressed and moves the player accordingly : 
+;RightArrow = Right, LeftArrow = Left, esc = exit the program, 
+;the LeftShift = Up is checked in adifferent way to allow Left/Right while Up 
+proc Movement
+	
+	push ax
+	push bx
+	push cx
+	push dx
+	
+	mov ax, 0
+	xor bx,bx
+	xor cx, cx
+	xor dx,dx
+	
+	mov ah, 1
+	int 16h ; checks if there is input, if there isn't zero flag == 1
+	
+	jnz DontJumpEnding
+	jmp Ending
+	
+DontJumpEnding:
+;	mov ah, 0
+;	int 16h ;takes user input (as hex scan code) and puts it in ah
+	;RightArrow = Right, LeftArrow = Left, esc = exit the program, LeftShift = Up (checked later in another way to enable Up and Right/Left at the same time) 
+	cmp ah, 4Dh
+	je Right
+	cmp ah, 4Bh
+	je Left
+	cmp ah, 1h ; esc button
+	je Close
+
+	jmp Ending
+
+Right:
+		;if the map dot to the Right of the player is a block (1), doesnt move Right
+	mov ax, 80
+	mul [playerY]
+	add ax, [playerX] ; makes ax the amount of offset in the map array the player is in
+	inc ax ; adds one to get the map dot Right to the player
+	add ax, offset map
+	mov bx, ax
+	cmp [byte ptr bx], 1
+	je Ending ; if the map dot to the Right is a block dont jump
+	
+	inc [playerX]
+	jmp Ending
+Left:
+		;if the map dot to the Left of the player is a block (1), doesnt move Left
+	mov ax, 80
+	mul [playerY]
+	add ax, [playerX] ; makes ax the amount of offset in the map array the player is in
+	dec ax ; decreaces one to get the map dot Left to the player
+	add ax, offset map
+	mov bx, ax
+	cmp [byte ptr bx], 1
+	je Ending ; if the map dot to the Left is a block dont jump
+	
+	dec [playerX]
+	jmp Ending
+
+Close: ;Closes program
+	jmp exit
+Up:
+	cmp [playerY], 2
+	jne DontWin
+	call Win
+DontWin:	
+	cmp [jumpTurns], 0 ;if out of jumpTurns doesnt move Up
+	je AfterUpCheck
+						;else if there are jumpTurns check if the block above is a block if not move the player
+
+	
+	cmp [jumpTurns], 5
+	jne NotStartJump
+	cmp [turnsInAir], 1
+	jg AfterUpCheck ; if the map dot below is air dont start jump
+	;dont start jump if player is in mid air
+NotStartJump:	
+
+	dec [jumpTurns]
+	
+	mov ax, 80
+	mul [playerY]
+	add ax, [playerX] ; makes ax the amount of offset in the map array the player is in
+	add ax, offset map
+	mov bx, ax ;gets player map dot location in bx
+	cmp [byte ptr bx], 1
+	je AfterUpCheck ; if the map dot the player is in is a block dont jump
+	
+	sub bx, 80 ; subtract 80 to get the map dot above to the player
+	cmp [byte ptr bx], 1
+	je AfterUpCheck ; if the map dot above is a block dont jump
+	
+	sub [playerY], 2 ;moves two to move one Up with Gravity
+	jmp AfterUpCheck
+
+Ending:
+	mov ah,2
+    int 16h     ; GET SHIFT FLAGS
+    test al,2   ; checks if LeftShift is pressed
+    jnz Up
+	
+AfterUpCheck:
+	mov ah, 0ch; interUpt that cleans keyboard buffer
+	int 21h
+	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+endp Movement
+
+;applys Gravity if player is in the air or resets jump parameters if player is on ground
+proc GravityAndGround
+	mov ax, 80
+	mul [playerY]
+	add ax, [playerX] ; makes ax the amount of offset in the map array the player is in
+	add ax, 80 ; add 80 to get the map dot below to the player
+	add ax, offset map
+	mov bx, ax
+	cmp [byte ptr bx], 1
+	je RestoreJumpTurnsAndgravityFlag; if the map dot below is a block restore jumpTurns and dont apply Gravity
+	
+Gravity:		;else apply Gravity
+	inc [turnsInAir]
+	inc [gravityFlag]
+	
+	cmp [gravityFlag], 4
+	jne EndOfTheFunction
+	
+	inc [playerY] ;move the player one down "Gravity"
+	mov [gravityFlag], 0
+	jmp EndOfTheFunction
+	
+RestoreJumpTurnsAndgravityFlag:
+	mov [gravityFlag], 0
+	mov [jumpTurns], 5
+	mov [turnsInAir], 0
+
+EndOfTheFunction:	
+	ret
+endp GravityAndGround
+
+;delay the program by 0.055 seconds by Alon Sarel
+proc Delay0_05Sec 
+	push ax
+	push cx
+	push es
+	
+	Clock equ es:6Ch
+	mov ax,40h
+	mov es,ax
+	mov ax,[Clock]
+FirstTick:
+	cmp ax,[Clock]
+	je FirstTick
+
+	pop es
+	pop cx
+	pop ax
+	ret
+endp Delay0_05Sec
+
+start:
+	mov ax, @data
+	mov ds, ax
+	
+	xor ax, ax
+
+	call GraphicsMode
+	
+	mov ax, offset startfile ; prints the start screen(start.bmp)
+	call PrintBmp
+
+	;Wait for key press
+	mov ah,1
+	int 21h
+	
+	mov ah,0
+	mov al,2 ; switches to text mode
+	int 10h
+	
+	call GraphicsMode
+	
+InfiniteLoop: ;Game loop
+	call PrintMap
+	call Delay0_05Sec ;delays to allow the player to see the changes and react accordingly
+	call RemovePlayer
+	call GravityAndGround
+	call Movement
+	call AddPlayer
+	jmp InfiniteLoop
+	
+exit:
+	mov ax, 4c00h
+	int 21h
+END start
